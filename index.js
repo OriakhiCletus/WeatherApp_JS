8741aa508106495cde644b96e45f354aac604945
Cletus Oriakhi <cletusidehen@gmail.com>
2024-10-31 08:02:20 +0100

    main codebase done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //weatherApp
 
+const { desc } = require("framer-motion/client");
+
 //needed variables
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
@@ -46,9 +48,10 @@ async function getWeatherData(city){
 
 }
 
-//function to display the weather info
+//function to display the weather info in the card after the user searches
 function displayWeatherInfo(data){
 
+    //destructure the JSON data to extra pieces of weather data and store them in variables
     const {name: city, 
            main: {temp, humidity},
            weather: [{description, id}]} = data;
@@ -61,10 +64,53 @@ function displayWeatherInfo(data){
     const humidityDisplay = document.createElement("p");
     const descDisplay = document.createElement("p");
     const weatherEmoji = document.createElement("p");
+
+    //add data to the html elements
+    cityDisplay.textContent = city;
+    tempDisplay.textContent = `${(temp - 273.15).toFixed(1)}°C`; //alter the formula to display it in Celsius(default is kelvin)
+    humidityDisplay.textContent = `Humidity: ${humidity}%`;
+    descDisplay.textContent = description;
+    weatherEmoji.textContent = getWeatherEmoji(id);
+
+    //add css styling using appropriate class
+    cityDisplay.classList.add("cityDisplay"); 
+    tempDisplay.classList.add("tempDisplay");
+    humidityDisplay.classList.add("humidityDisplay");
+    descDisplay.classList.add("descDisplay");
+    weatherEmoji.classList.add("weatherEmoji");
+
+    //add the data to the card to display it
+    card.appendChild(cityDisplay); 
+    card.appendChild(tempDisplay);
+    card.appendChild(humidityDisplay);
+    card.appendChild(descDisplay);
+    card.appendChild(weatherEmoji);
+
 }
 
 //get weather emoji based on what the weather is
 function getWeatherEmoji(weatherId) {
+    
+    switch(true){
+        case (weatherId >= 200 && weatherId < 300):
+            return "⛈️";
+        case (weatherId >= 300 && weatherId < 400):
+            return "🌧️";
+        case (weatherId >= 500 && weatherId < 600):
+            return "🌧️";
+        case (weatherId >= 600 && weatherId < 700):
+            return "❄️";
+         case (weatherId >= 700 && weatherId < 800):
+            return "❄️";
+         case (weatherId >= 700 && weatherId < 800):
+            return "❄️";
+         case (weatherId === 800):
+            return "☀️";
+        case (weatherId >= 801 && weatherId < 810):
+            return "❄️";
+        default:
+            return "❓";
+    }
 
 }
 
